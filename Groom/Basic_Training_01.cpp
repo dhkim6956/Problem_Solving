8e432b6396af9b79a35eca91e24eb6cdfdc8838b
dhkim6956 <dhkim6956@naver.com>
2023-11-30 11:10:49 +0900

    ✏Solved : Groom Basic 3

---
+++ Groom/Basic_Training_01.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    char target[101];
+    int cnt = 0;
+
+    cin >> n >> target;
+
+    for (int i = 0; i < n; i++) {
+        char num[200001];
+
+        cin >> num;
+
+        char* result = strstr(num, target);
+
+        if (result) {
+            continue;
+        }
+        else {
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+
+    return 0;
+}
---
+++ Groom/Basic_Training_02.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    long long sum = 1;
+
+    for (int i = 1; i <= n; i++) {
+        sum = sum * i % 1000000007;
+    }
+
+    cout << sum;
+
+    return 0;
+}
